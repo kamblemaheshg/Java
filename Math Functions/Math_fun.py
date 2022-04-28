a94c9b6acdfed6be8d434c6a4d323f582b320011
Mahesh Kamble <90838761+kamblemaheshg@users.noreply.github.com>
2022-04-28 19:19:05 +0530

    Create Math_fun.py

---
+++ Math Functions/Math_fun.py
@@ -0,0 +1,11 @@
+import math
+pi =3.14
+print(round(pi))
+print(math.ceil(pi))
+print(math.floor(pi))
+print(abs(pi)) #how for number from 0
+print(pow(pi,2))
+print(math.sqrt(144))
+x , y , z = 1,23,98
+print(max(x,y,z))
+print(min(x,y,z))
