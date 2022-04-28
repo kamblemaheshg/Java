d504a5716d81465e3d8b23273ebf5ef00ce39b8b
Mahesh Kamble <90838761+kamblemaheshg@users.noreply.github.com>
2022-04-28 18:54:15 +0530

    Create Typecast.py

---
+++ Type_Cast/Typecast.py
@@ -0,0 +1,29 @@
+# python is dynamically typed languGE HENCE WE DONT NEED TO SPECIFY  variable type_repr
+#string to int 
+import string
+
+
+name = '10'
+print(int(name))
+print(float(name))
+print(bool(name))
+
+
+
+#int
+age = 19 
+print(str(age))
+
+#float
+height = 189.23
+print(bool(height))
+print(int(height))
+print(str(height))
+
+
+
+#boolean
+flag = True
+print(type(flag))
+print(int(flag))
+print(str(flag))
