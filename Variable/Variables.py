c443aef19395a3c8f4d0da311e995850c3f6bc8a
Mahesh Kamble <90838761+kamblemaheshg@users.noreply.github.com>
2022-04-28 18:45:42 +0530

    Create Variables.py

---
+++ Variable/Variables.py
@@ -0,0 +1,26 @@
+# python is dynamically typed languGE HENCE WE DONT NEED TO SPECIFY  variable type_repr
+#string
+name = "Name"
+print(type(name))
+print(name+"Hi")
+
+#int
+age = 19 
+print(type(age))
+
+#float
+height = 189.23
+print(type(height))
+
+#boolean
+flag = True
+print(type(flag))
+
+# multiple assign = allows to assign multi variables at same time in one line of code 
+name , age , school = "Mahesh" , 19 , "JNV"
+print(name)
+print(age)
+print(school)
+
+
+
