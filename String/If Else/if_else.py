1c9a60e4dc3a0d8ca8fc49db1c93f18e1623e34a
Mahesh Kamble <90838761+kamblemaheshg@users.noreply.github.com>
2022-04-28 21:06:33 +0530

    Create if_else.py

---
+++ String/If Else/if_else.py
@@ -0,0 +1,9 @@
+age = int(input("How old are you ? : "))
+if age>=18 and age<50 :
+    print("You are adult!")
+elif age>=50 and age <70 :
+    print("You are old!")
+elif age > 1 and age>10:
+    print("You are Child!")
+else:
+    print("who are you")
