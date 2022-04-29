e47281e1ac94b908c09c4f432d122632b522ce92
Mahesh Kamble <90838761+kamblemaheshg@users.noreply.github.com>
2022-04-29 18:41:36 +0530

    Create List2D.py

---
+++ List/List2D.py
@@ -0,0 +1,7 @@
+drink=["Coca cola","soda","tea"]
+dinner=["Pizza","Chappati","Bhakar"]
+dessert=["cake","Ice-cream","cream"]
+food=[drink,dinner,dessert]
+print(food)
+print(food[1])
+print(food[0][2])
